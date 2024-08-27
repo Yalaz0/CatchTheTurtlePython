2f1e37733a91d46928956f66e7f09b850d827a4c
yalaz <yalazmehmetjs@gmail.com>
2024-08-27 18:05:40 +0300

    atil samancıoğlu versiyon

--- main.py
+++ main.py
@@ -1,76 +1,122 @@
-import turtle
 import random
-import time
+import turtle
 
-# Ekranı oluştur ve yapılandır
-ekran = turtle.Screen()
-ekran.title("Tıklama Oyunu")
-ekran.bgcolor("light blue")
-ekran.setup(width=700, height=650)
+screen = turtle.Screen()
+game_over = False
+score = 0
+FONT = ('Arial', 30, 'normal')
 
-# Skor ve sayaç için global değişkenler
-skor = 0
-sure = 30
+#turtle list
+turtle_list = []
 
-# Skor tablosu
-skor_tahtasi = turtle.Turtle()
-skor_tahtasi.hideturtle()
-skor_tahtasi.penup()
-skor_tahtasi.goto(-250, 250)
-skor_tahtasi.write(f"Skor: {skor}", align="left", font=("Arial", 16, "normal"))
+#countdown turtle
+count_down_turtle = turtle.Turtle()
 
-# Sayaç
-sayaç_tahtasi = turtle.Turtle()
-sayaç_tahtasi.hideturtle()
-sayaç_tahtasi.penup()
-sayaç_tahtasi.goto(200, 250)
-sayaç_tahtasi.write(f"Süre: {sure}", align="left", font=("Arial", 16, "normal"))
+# score turtle
+score_turtle = turtle.Turtle()
 
-# Tıklanacak kaplumbağa nesnesi
-tıklanacak_turtle = turtle.Turtle()
-tıklanacak_turtle.shape("turtle")
-tıklanacak_turtle.color("green")
-tıklanacak_turtle.penup()
-tıklanacak_turtle.speed(0)
+def setup_score_turtle():
+    score_turtle.hideturtle()
+    score_turtle.color("blue")
+    score_turtle.penup()
 
-# Skor güncelleme fonksiyonu
-def skor_guncelle():
-    global skor
-    skor += 1
-    skor_tahtasi.clear()
-    skor_tahtasi.write(f"Skor: {skor}", align="left", font=("Arial", 16, "normal"))
+    top_height = screen.window_height() / 2  # positive height/2 is the top of the screen
+    y = top_height - top_height / 10  # decreasing a bit so text will be visible
+    score_turtle.setposition(0, y)
+    score_turtle.write(arg='Score: 0', move=False, align='center', font=FONT)
 
-# Sayaç güncelleme fonksiyonu
-def sayaç_guncelle():
-    global sure
-    sure -= 1
-    sayaç_tahtasi.clear()
-    sayaç_tahtasi.write(f"Süre: {sure}", align="left", font=("Arial", 16, "normal"))
+grid_size = 10
 
-# Kaplumbağanın konumunu rastgele olarak güncelle
-def konum_guncelle():
-    x = random.randint(-250, 250)
-    y = random.randint(-200, 200)
-    tıklanacak_turtle.goto(x, y)
+def make_turtle(x, y):
+    t = turtle.Turtle()
 
-# Tıklama işlemi
-def tıklama_islemi(x, y):
-    if sure > 0:
-        skor_guncelle()
-        konum_guncelle()
+    def handle_click(x, y):
+        global score
+        score += + 1
+        score_turtle.clear()
+        score_turtle.write("Score: {}".format(score), move=False, align="center", font=FONT)
+        print(x, y)
 
-# Tıklama olayını dinle
-tıklanacak_turtle.onclick(tıklama_islemi)
+    t.onclick(handle_click)
+    t.penup()
+    t.shape("turtle")
+    t.shapesize(2, 2)
+    t.color("green")
+    t.goto(x * grid_size, y * grid_size)
+    t.pendown()
+    turtle_list.append(t)
 
-while sure > 0:
-    konum_guncelle()
-    time.sleep(1)
-    sayaç_guncelle()
 
-# Oyun bitti
-sayaç_tahtasi.clear()
-sayaç_tahtasi.write("Oyun Bitti!", align="left", font=("Arial", 16, "normal"))
-tıklanacak_turtle.hideturtle()
+x_coordinates = [-20, -10, 0, 10, 20]
+y_coordinates = [20, 10, 0, -10]
 
-# Ekranı kapat
-ekran.mainloop()
+def setup_turtles():
+    for x in x_coordinates:
+        for y in y_coordinates:
+            make_turtle(x, y)
+    '''
+    make_turtle(-20,20)
+    make_turtle(-10,20)
+    make_turtle(-0,20)
+    make_turtle(10,20)
+    make_turtle(20,20)
+    make_turtle(-20,10)
+    make_turtle(-10,10)
+    make_turtle(-0,10)
+    make_turtle(10,10)
+    make_turtle(20,10)
+    make_turtle(-20,0)
+    make_turtle(-10,0)
+    make_turtle(-0,0)
+    make_turtle(10,0)
+    make_turtle(20,0)
+    make_turtle(-20,-10)
+    make_turtle(-10,-10)
+    make_turtle(-0,-10)
+    make_turtle(10,-10)
+    make_turtle(20,-10)
+    '''
+
+def hide_turtles():
+    for t in turtle_list:
+        t.hideturtle()
+
+def show_turtles_randomly():
+    if not game_over:
+        hide_turtles()
+        random.choice(turtle_list).showturtle()
+        screen.ontimer(show_turtles_randomly, 500)
+
+
+def countdown(time):
+    global game_over
+    top_height = screen.window_height() / 2
+    y = top_height - top_height / 10
+    count_down_turtle.hideturtle()
+    count_down_turtle.penup()
+    count_down_turtle.setposition(0, y - 30)
+    count_down_turtle.clear()
+
+    if time > 0:
+        count_down_turtle.clear()
+        count_down_turtle.write("Time: {}".format(time),move=False,align="center",font=FONT)
+        screen.ontimer(lambda: countdown(time - 1), 1000)
+    else:
+        game_over = True
+        count_down_turtle.clear()
+        hide_turtles()
+        count_down_turtle.write("Game Over!", align='center', font=FONT)
+
+def start_game_up():
+    global game_over
+    game_over = False
+    turtle.tracer(0)
+    setup_score_turtle()
+    setup_turtles()
+    hide_turtles()
+    show_turtles_randomly()
+    turtle.tracer(1)
+    screen.ontimer(lambda: countdown(10), 10)
+
+start_game_up()
+turtle.mainloop()
